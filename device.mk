6e8fa9fde8a7c059c30300d35c29e89a35f28911
Leonel Artuzi <leonel.artu@gmail.com>
2023-08-28 09:47:38 -0300

    j4primelte: overlay: Replies in notification don't requires
    * Define LockScreen Display TimeOut
    
    Change-Id: Ib5814321e33ecb9d7a26d1bd3fd109c40ece8c96

--- device.mk
+++ device.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Inherit from msm8917-common
 $(call inherit-product, device/samsung/msm8917-common/msm8917.mk)
 
